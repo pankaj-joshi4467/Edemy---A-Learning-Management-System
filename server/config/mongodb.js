9ed3f16535f69a4fb630e4d203e2fd2277dc3f8c
pankaj <pankajjoshi4467@gmail.com>
2025-02-21 00:15:33 +0530

    first commit

---
+++ server/config/mongodb.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+
+// Connect to MongoDB
+
+const connectDB = async () => {
+    mongoose.connection.on("connected", () => {
+        console.log("Database connected");
+    })
+    await mongoose.connect(`${process.env.MONGODB_URI}/lms` )
+}
+
+export default connectDB;
---
+++ server/server.js
@@ -0,0 +1,36 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+import { clerkWebhooks } from './controllers/webhooks.js'
+
+// Initialize express
+const app = express()
+
+// connect to database
+await connectDB()
+
+
+// Middleware
+app.use(cors())
+
+
+// Routes
+app.get('/',(req,res)=>res.send("API Working"))
+
+app.post('/clerk',express.json(),clerkWebhooks)
+
+
+// Port
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`)
+})
+
+
+
+
+
+
+
